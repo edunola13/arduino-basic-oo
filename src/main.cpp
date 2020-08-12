fcfb96694e975a53bb2ca87ac69a245bceb010fc
Eduardo Nola <edunola13@hotmail.com>
2020-08-12 00:15:31 -0300

    Comment Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,28 @@
-#include <ComponentAbstract.h>
-#include <JsonHelper.h>
-
-#include <sensors/AnalogSensor.h>
-#include <sensors/DigitalSensor.h>
-#include <sensors/DistanceHCSR04.h>
-#include <sensors/HumTempDHT.h>
-#include <sensors/TempBase.h>
-#include <sensors/BMPSensor.h>
-
-#include <actuators/DigitalControl.h>
-#include <actuators/Relay.h>
-
-AnalogSensor sen1(0);
-RainSensor rain(0);
-LdrsSensor ldrs(0);
-MQSensor mq(0);
-DigitalSensor dig(2);
-DistanceHCSR04 dis(3, 4);
-HumTempDHT sen(5, 11);
-TempBase tmp(2, 1);
-BMPSensor bmp(25, 0);
-
-DigitalControl digC(3,1);
-Relay rel(3);
+// #include <ComponentAbstract.h>
+// #include <JsonHelper.h>
+//
+// #include <sensors/AnalogSensor.h>
+// #include <sensors/DigitalSensor.h>
+// #include <sensors/DistanceHCSR04.h>
+// #include <sensors/HumTempDHT.h>
+// #include <sensors/TempBase.h>
+// #include <sensors/BMPSensor.h>
+//
+// #include <actuators/DigitalControl.h>
+// #include <actuators/Relay.h>
+//
+// AnalogSensor sen1(0);
+// RainSensor rain(0);
+// LdrsSensor ldrs(0);
+// MQSensor mq(0);
+// DigitalSensor dig(2);
+// DistanceHCSR04 dis(3, 4);
+// HumTempDHT sen(5, 11);
+// TempBase tmp(2, 1);
+// BMPSensor bmp(25, 0);
+//
+// DigitalControl digC(3,1);
+// Relay rel(3);
 
 void setup() {
   // put your setup code here, to run once:
